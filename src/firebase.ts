b6bf2d44599f8a790d000a9ef4688e47c02bac3d
Tatiana <kozachenko8902@gmail.com>
2025-11-14 14:46:14 +0000

    firebase info

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 // TODO: Вставте сюди ваші реальні налаштування Firebase
 // Ви можете отримати їх з консолі Firebase > Project Settings
 const firebaseConfig = {
-  apiKey: "AIza...",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyDcczqnIscPxZzIozvyvlpEMdF3KA_LxMI",
+  authDomain: "fopilot-e21f3.firebaseapp.com",
+  projectId: "fopilot-e21f3",
+  storageBucket: "fopilot-e21f3.firebasestorage.app",
+  messagingSenderId: ".1017700850121",
+  appId: "1:1017700850121:web:4f6f3ba75ab0f36887b018"
+  //measurementId: "G-JVCGE5PJRG"
 };
 
 // Ініціалізуємо Firebase
